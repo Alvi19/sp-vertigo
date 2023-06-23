7ae001805d25e54e3d2a4a7744394e33d3b1818c
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2023-06-23 14:18:48 +0700

    fix db

--- database/sp_vertigo.sql
+++ database/sp_vertigo.sql
@@ -1,13 +1,10 @@
 --
 -- Database: `sp_vertigo`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `admin`
 --
-
 CREATE TABLE `admin` (
   `id` int NOT NULL,
   `username` varchar(25) NOT NULL,
@@ -18,16 +15,20 @@ CREATE TABLE `admin` (
 --
 -- Dumping data untuk tabel `admin`
 --
-
-INSERT INTO `admin` (`id`, `username`, `password`, `last_login`) VALUES
-(1, 'admin', '$2y$10$hRi1qju2KOeEPcBZ0wYfhu/PN5e9Wl.ddWeDTds8Uokad764X9D1a', '2023-06-14 07:33:34');
+INSERT INTO
+  `admin` (`id`, `username`, `password`, `last_login`)
+VALUES
+  (
+    1,
+    'admin',
+    '$2y$10$hRi1qju2KOeEPcBZ0wYfhu/PN5e9Wl.ddWeDTds8Uokad764X9D1a',
+    '2023-06-14 07:33:34'
+  );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `detail_hasil`
 --
-
 CREATE TABLE `detail_hasil` (
   `id` int NOT NULL,
   `id_penyakit` int NOT NULL,
@@ -35,11 +36,9 @@ CREATE TABLE `detail_hasil` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `gejala`
 --
-
 CREATE TABLE `gejala` (
   `id` int NOT NULL,
   `nama_gejala` varchar(255) NOT NULL,
@@ -47,11 +46,9 @@ CREATE TABLE `gejala` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `hasil`
 --
-
 CREATE TABLE `hasil` (
   `id` int NOT NULL,
   `id_pasien` int NOT NULL,
@@ -62,11 +59,9 @@ CREATE TABLE `hasil` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `kategori`
 --
-
 CREATE TABLE `kategori` (
   `id` int NOT NULL,
   `kode_kategori` varchar(45) NOT NULL,
@@ -75,11 +70,9 @@ CREATE TABLE `kategori` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `pasien`
 --
-
 CREATE TABLE `pasien` (
   `id` int NOT NULL,
   `nama` varchar(100) NOT NULL,
@@ -88,11 +81,9 @@ CREATE TABLE `pasien` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `penyakit`
 --
-
 CREATE TABLE `penyakit` (
   `id` int NOT NULL,
   `id_kategori` int NOT NULL,
@@ -102,11 +93,9 @@ CREATE TABLE `penyakit` (
 );
 
 -- --------------------------------------------------------
-
 --
 -- Struktur dari tabel `pertanyaan`
 --
-
 CREATE TABLE `pertanyaan` (
   `id` int NOT NULL,
   `pertanyaan` varchar(255) NOT NULL,
@@ -116,104 +105,136 @@ CREATE TABLE `pertanyaan` (
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indeks untuk tabel `admin`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `admin`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `detail_hasil`
 --
-ALTER TABLE `detail_hasil`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `detail_hasil`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `gejala`
 --
-ALTER TABLE `gejala`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `gejala`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `hasil`
 --
-ALTER TABLE `hasil`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `hasil`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `kategori`
 --
-ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `kategori`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `pasien`
 --
-ALTER TABLE `pasien`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `pasien`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `penyakit`
 --
-ALTER TABLE `penyakit`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_kategori` (`id_kategori`);
+ALTER TABLE
+  `penyakit`
+ADD
+  PRIMARY KEY (`id`),
+ADD
+  UNIQUE KEY `id_kategori` (`id_kategori`);
 
 --
 -- Indeks untuk tabel `pertanyaan`
 --
-ALTER TABLE `pertanyaan`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE
+  `pertanyaan`
+ADD
+  PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
-
 --
 -- AUTO_INCREMENT untuk tabel `admin`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE
+  `admin`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 2;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_hasil`
 --
-ALTER TABLE `detail_hasil`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `detail_hasil`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `gejala`
 --
-ALTER TABLE `gejala`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `gejala`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `hasil`
 --
-ALTER TABLE `hasil`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `hasil`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
-ALTER TABLE `kategori`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `kategori`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `pasien`
 --
-ALTER TABLE `pasien`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `pasien`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `penyakit`
 --
-ALTER TABLE `penyakit`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `penyakit`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `pertanyaan`
 --
-ALTER TABLE `pertanyaan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE
+  `pertanyaan`
+MODIFY
+  `id` int NOT NULL AUTO_INCREMENT;
