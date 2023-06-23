416405b84724d8a5038c23360a918c806f8ab5ac
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2023-06-23 14:15:24 +0700

    fix db

--- database/sp_vertigo.sql
+++ database/sp_vertigo.sql
@@ -1,22 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Waktu pembuatan: 14 Jun 2023 pada 13.03
--- Versi server: 8.0.30
--- Versi PHP: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `sp_vertigo`
 --
@@ -29,10 +10,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int NOT NULL,
-  `username` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `last_login` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Dumping data untuk tabel `admin`
@@ -51,7 +32,7 @@ CREATE TABLE `detail_hasil` (
   `id` int NOT NULL,
   `id_penyakit` int NOT NULL,
   `presentase` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -63,7 +44,7 @@ CREATE TABLE `gejala` (
   `id` int NOT NULL,
   `nama_gejala` varchar(255) NOT NULL,
   `bobot` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -78,7 +59,7 @@ CREATE TABLE `hasil` (
   `bobot` double NOT NULL,
   `tanggal` timestamp NOT NULL,
   `kriteria` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -91,7 +72,7 @@ CREATE TABLE `kategori` (
   `kode_kategori` varchar(45) NOT NULL,
   `nama_kategori` varchar(45) NOT NULL,
   `deskripsi` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -104,7 +85,7 @@ CREATE TABLE `pasien` (
   `nama` varchar(100) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `JK` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -116,9 +97,9 @@ CREATE TABLE `penyakit` (
   `id` int NOT NULL,
   `id_kategori` int NOT NULL,
   `solusi` varchar(255) NOT NULL,
-  `nama_penyakit` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nama_penyakit` varchar(255) NOT NULL,
   `deskripsi` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -130,7 +111,7 @@ CREATE TABLE `pertanyaan` (
   `id` int NOT NULL,
   `pertanyaan` varchar(255) NOT NULL,
   `id_gejala` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Indexes for dumped tables
@@ -235,19 +216,4 @@ ALTER TABLE `penyakit`
 -- AUTO_INCREMENT untuk tabel `pertanyaan`
 --
 ALTER TABLE `pertanyaan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `penyakit`
---
-ALTER TABLE `penyakit`
-  ADD CONSTRAINT `penyakit_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
